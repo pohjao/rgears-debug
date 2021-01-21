be4bf1b55d9dbb93d48318ca7a21481f293fa455
Oskari Pohja <oskari.pohja@nsion.fi>
2021-01-21 15:59:03 +0200

    init

---
+++ gear.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42be7362eb898a79285024abf6ab46cc264e3dfe373805376bdc56795328d2b0
+size 1892
---
+++ run.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c388207e6c5d38a64910f1223b9b414e4ad392394a988231c84734e17d9044f0
+size 1058
